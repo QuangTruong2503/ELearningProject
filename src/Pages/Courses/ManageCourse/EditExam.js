b6eebbfed81d1990aba85ba3250f3c045d6eb3bf
Trần Vũ Quang Trường <trutruong2503@gmail.com>
2024-12-24 15:08:11 +0700

    update

--- src/Pages/Courses/ManageCourse/EditExam.js
+++ src/Pages/Courses/ManageCourse/EditExam.js
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import LoaderButton from "../../../Component/Loader/LoaderButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Tab, Nav } from "react-bootstrap";
 
 function EditExam() {
   const { examID } = useParams();
@@ -21,6 +22,7 @@ function EditExam() {
     finished_at: "",
     course_id: "",
   });
+
   // Hàm xử lý thay đổi dữ liệu
   const handleChange = (e) => {
     const { id, value, type } = e.target;
@@ -62,6 +64,7 @@ function EditExam() {
       setIsLoading(false);
     }
   };
+
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-start">
@@ -73,126 +76,143 @@ function EditExam() {
         </button>
       </div>
       <h3 className="mb-4 text-center">Chỉnh Sửa Bài Kiểm Tra</h3>
-      <form
-        className="d-flex flex-column gap-1"
-        onSubmit={handleUpdateExam}
-        onReset={() => setReload(!reload)}
-      >
-        <div className="row mb-3">
-          <div className="col-md-12">
-            <label htmlFor="exam_name" className="form-label">
-              Tên Kiểm Tra
-            </label>
-            <input
-              type="text"
-              id="exam_name"
-              className="form-control"
-              value={examDetail.exam_name}
-              required
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <div className="col-md-6">
-            <label htmlFor="total_score" className="form-label">
-              Tổng Điểm
-            </label>
-            <input
-              type="number"
-              id="total_score"
-              className="form-control"
-              required
-              value={examDetail.total_score}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="exam_time" className="form-label">
-              Thời Gian Làm Bài (phút)
-            </label>
-            <input
-              type="number"
-              id="exam_time"
-              className="form-control"
-              required
-              value={examDetail.exam_time}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <div className="col-md-6">
-            <label htmlFor="created_at" className="form-label">
-              Ngày Tạo
-            </label>
-            <input
-              type="text"
-              id="created_at"
-              className="form-control"
-              value={new Date(examDetail.created_at).toLocaleString()}
-              disabled
-            />
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="finished_at" className="form-label">
-              Ngày Kết Thúc
-            </label>
-            <input
-              type="datetime-local"
-              id="finished_at"
-              className="form-control"
-              value={(examDetail.finished_at).slice(0, 16)}
-              onChange={handleChange}
-            />
-            <small>
-              Học sinh sẽ không thể truy cập vào bài thi sau khi kết thúc.
-            </small>
-          </div>
-        </div>
-        <div className="row mb-3">
-          <div className="col-md-6">
-            <label htmlFor="hide_result" className="form-label">
-              Kết Quả
-            </label>
-            <select
-              id="hide_result"
-              className="form-select"
-              value={examDetail.hide_result}
-              required
-              onChange={(e) =>
-                setExamDetail((prevData) => ({
-                  ...prevData,
-                  hide_result: e.target.value === "true",
-                }))
-              }
+      <Tab.Container defaultActiveKey="generalInfo">
+        <Nav variant="tabs" className="mb-3">
+          <Nav.Item>
+            <Nav.Link eventKey="generalInfo">Thông Tin Chung</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="questions">Câu Hỏi</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content>
+          {/* Trang 1: Thông Tin Chung */}
+          <Tab.Pane eventKey="generalInfo">
+            <form
+              className="d-flex flex-column gap-1"
+              onSubmit={handleUpdateExam}
+              onReset={() => setReload(!reload)}
             >
-              <option value="false">Hiển Thị</option>
-              <option value="true">Ẩn</option>
-            </select>
-            <small>
-              Kết quả sau khi hoàn thành kiểm tra sẽ hiện thị hoặc ẩn.
-            </small>
-          </div>
-        </div>
-        <div className="d-flex justify-content-center gap-2">
-          <button type="reset" className="btn btn-secondary">
-            Đặt Lại
-          </button>
-          {/* Nút lưu */}
-          {isLoading ? (
-            <button type="button" disabled className="btn btn-primary me-2">
-              <LoaderButton />
-            </button>
-          ) : (
-            <button type="submit" className="btn btn-primary me-2">
-              Lưu Thay Đổi
-            </button>
-          )}
-        </div>
-      </form>
-      {/* Questions */}
-      <ExamQuestions examData={examDetail} />
+              <div className="row mb-3">
+                <div className="col-md-12">
+                  <label htmlFor="exam_name" className="form-label">
+                    Tên Kiểm Tra
+                  </label>
+                  <input
+                    type="text"
+                    id="exam_name"
+                    className="form-control"
+                    value={examDetail.exam_name}
+                    required
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label htmlFor="total_score" className="form-label">
+                    Tổng Điểm
+                  </label>
+                  <input
+                    type="number"
+                    id="total_score"
+                    className="form-control"
+                    required
+                    value={examDetail.total_score}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label htmlFor="exam_time" className="form-label">
+                    Thời Gian Làm Bài (phút)
+                  </label>
+                  <input
+                    type="number"
+                    id="exam_time"
+                    className="form-control"
+                    required
+                    value={examDetail.exam_time}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label htmlFor="created_at" className="form-label">
+                    Ngày Tạo
+                  </label>
+                  <input
+                    type="text"
+                    id="created_at"
+                    className="form-control"
+                    value={new Date(examDetail.created_at).toLocaleString()}
+                    disabled
+                  />
+                </div>
+                <div className="col-md-6">
+                  <label htmlFor="finished_at" className="form-label">
+                    Ngày Kết Thúc
+                  </label>
+                  <input
+                    type="datetime-local"
+                    id="finished_at"
+                    className="form-control"
+                    value={examDetail.finished_at.slice(0, 16)}
+                    onChange={handleChange}
+                  />
+                  <small>
+                    Học sinh sẽ không thể truy cập vào bài thi sau khi kết thúc.
+                  </small>
+                </div>
+              </div>
+              <div className="row mb-3">
+                <div className="col-md-6">
+                  <label htmlFor="hide_result" className="form-label">
+                    Kết Quả
+                  </label>
+                  <select
+                    id="hide_result"
+                    className="form-select"
+                    value={examDetail.hide_result}
+                    required
+                    onChange={(e) =>
+                      setExamDetail((prevData) => ({
+                        ...prevData,
+                        hide_result: e.target.value === "true",
+                      }))
+                    }
+                  >
+                    <option value="false">Hiển Thị</option>
+                    <option value="true">Ẩn</option>
+                  </select>
+                  <small>
+                    Kết quả sau khi hoàn thành kiểm tra sẽ hiện thị hoặc ẩn.
+                  </small>
+                </div>
+              </div>
+              <div className="d-flex justify-content-center gap-2">
+                <button type="reset" className="btn btn-secondary">
+                  Đặt Lại
+                </button>
+                {isLoading ? (
+                  <button type="button" disabled className="btn btn-primary me-2">
+                    <LoaderButton />
+                  </button>
+                ) : (
+                  <button type="submit" className="btn btn-primary me-2">
+                    Lưu Thay Đổi
+                  </button>
+                )}
+              </div>
+            </form>
+          </Tab.Pane>
+
+          {/* Trang 2: Câu Hỏi */}
+          <Tab.Pane eventKey="questions">
+            <ExamQuestions examData={examDetail} />
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
     </div>
   );
 }
